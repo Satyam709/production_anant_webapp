1d6325484261137382e9ee1431b7107519b75cc9
= <satyamchauhan709@gmail.com>
2024-12-28 02:42:23 +0530

    added next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import Provider from "@/lib/Providers/Provider";
+import authOptions from "@/lib/NextAuth/authOptions";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: "Anant: Next App",
   description: "Anant Next.js App",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Provider session={session}>{children}</Provider>
+      </body>
     </html>
   );
 }
