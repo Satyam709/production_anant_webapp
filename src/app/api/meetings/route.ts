29c6f11212d388200fc08679b785e21cf920f6fa
= <satyamchauhan709@gmail.com>
2024-12-30 18:28:51 +0530

    added get upcoming and past meetings route

---
+++ src/app/api/meetings/route.ts
@@ -0,0 +1,61 @@
+import prisma from "@/lib/PrismaClient/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const pageSize = 3;
+    const url = new URL(req.url);
+    const searchParams = url.searchParams;
+
+    const time = searchParams.get("time");
+    const pageNumber = parseInt(searchParams.get("page") || "1", 10);
+    const currentDate = new Date();
+
+    let meetings;
+    console.log(time);
+    console.log(searchParams);
+    
+
+    // Fetch upcoming and past meetings
+    if (!time || time === "upcoming") {
+      const upcomingMeetings = await prisma.meeting.findMany({
+        where: {
+          starts: {
+            gte: currentDate,
+          },
+        },
+        take: pageSize,
+        skip: (pageNumber - 1) * pageSize,
+        orderBy: {
+          starts: "asc",
+        },
+      });
+      meetings = { upcoming: upcomingMeetings };
+    }
+
+    if (!time || time === "past") {
+      const pastMeetings = await prisma.meeting.findMany({
+        where: {
+          starts: {
+            lt: currentDate,
+          },
+        },
+        take: pageSize,
+        skip: (pageNumber - 1) * pageSize,
+        orderBy: {
+          starts: "desc",
+        },
+      });
+      meetings = { ...meetings, past: pastMeetings };
+    }
+
+    // Return the meetings in the response
+    return NextResponse.json({ meetings });
+  } catch (error) {
+    console.error("Error fetching meetings: ", error);
+    return NextResponse.json(
+      { error: "Failed to fetch meetings" },
+      { status: 500 }
+    );
+  }
+}
