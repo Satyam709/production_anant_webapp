54e8f73e0e09d5a393eb38f4b17ddbed16b102ca
= <satyamchauhan709@gmail.com>
2024-12-30 20:18:43 +0530

    added utility session functions

---
+++ src/lib/actions/Sessions.ts
@@ -0,0 +1,18 @@
+import { getServerSession } from "next-auth";
+import authOptions from "../NextAuth/authOptions";
+
+export async function getSession() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    console.error("unauthenticated!!");
+  }
+  return session;
+}
+
+export async function isAuthenticated() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return false;
+  }
+  return true;
+}
