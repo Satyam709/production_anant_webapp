7eff9e0025bce943bae1614fa7f8233d1a6f28fc
satyam709 <satyamchauhan709@gmail.com>
2025-02-05 15:36:23 +0530

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com',"example.com"], // Add the domain here
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
