ce9df28de872e00466563568a87dfd01b572feec
satyam709 <satyamchauhan709@gmail.com>
2025-02-23 19:36:30 +0530

    added admin protection on uploadthing

--- src/lib/actions/uploadthing.ts
+++ src/lib/actions/uploadthing.ts
@@ -1,5 +1,7 @@
-"use server"
+"use server";
 import { UTApi } from "uploadthing/server";
+import { getSession } from "./Sessions";
+import isAdmin from "./Admin";
 
 const utapi = new UTApi();
 
@@ -12,5 +14,49 @@ export async function uploadServerSideFile(file: File) {
   } catch (error) {
     console.error("Upload failed:", error);
     throw error;
+  }
+}
+
+export async function uploadServerSideFiles(files: File[]) {
+  try {
+    const response = await utapi.uploadFiles(files);
+    console.log("Upload complete for userId:", response);
+    // console.log("Upload complete for userId:", response.data?.ufsUrl);
+    return response;
+  } catch (error) {
+    console.error("Upload failed:", error);
+    throw error;
+  }
+}
+
+export async function uploadAdminServerSideFile(file: File) {
+  try {
+    const session = await getSession();
+    if (!session?.user || !(await isAdmin())) {
+      return null;
+    }
+    const response = await utapi.uploadFiles(file);
+    console.log("Upload complete for userId:", response);
+    console.log("Upload complete for userId:", response.data?.ufsUrl);
+    return response.data;
+  } catch (error) {
+    console.error("Upload failed:", error);
+    return null;
+  }
+}
+
+export async function uploadAdminServerSideFiles(files: File[]) {
+  try {
+    const session = await getSession();
+    if (!session?.user || !(await isAdmin())) {
+      return null;
+    }
+    const response = await utapi.uploadFiles(files);
+    console.log("Upload complete for userId:", response);
+    // console.log("Upload complete for userId:", response.data?.ufsUrl);
+    return response;
+  } catch (error) {
+    console.error("Upload failed:", error);
+    return null;
   }
 }
