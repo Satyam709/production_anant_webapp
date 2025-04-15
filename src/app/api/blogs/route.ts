51fa5ec4d821a2a31fac41401a6a9bfa3d33d2f2
Ayush <ayushsur26@gmail.com>
2025-04-15 12:49:39 +0530

    blogs fetch added

---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import prsima from "@/lib/PrismaClient/db"
+
+export async function GET(req:NextRequest){
+    try{
+        const size = 10;
+        const url = new URL(req.url);
+        const searchParams = url.searchParams;
+        const pageNumber = parseInt(searchParams.get("page") || "1", 10);
+     
+        if (pageNumber < 1) {
+            return NextResponse.json(
+              { message: "Invalid page number" },
+              { status: 400 }
+            );
+        }
+
+        const blogs = await prisma.blog.findMany({
+            where:{
+                isVerified: true
+            },
+            take: size,
+            skip: (pageNumber-1)-size,
+            orderBy:{
+                createdAt: "asc"
+            }
+        });
+
+        return NextResponse.json(
+            {blogs: blogs},{status:200}
+        )
+
+    }
+    catch(err){
+        console.log(err);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
